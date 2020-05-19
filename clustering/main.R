fa1f9e394e72a36a3e6880e3dd6fadfa90297c46
cvalienteDS <65561135+cvalienteDS@users.noreply.github.com>
2020-05-19 14:58:39 +0200

    Create main.R

---
+++ clustering/main.R
@@ -0,0 +1,20 @@
+# please set your working directory
+
+dataIN <- read.csv('toy_sample.csv', 
+                       header=T , 
+                       dec=".",
+                       sep=';', 
+                       encoding = "UTF-8",
+                       stringsAsFactors = F,
+                       na.strings=c("#N/D", "#VALOR","#¡VALOR!", "#VALOR!", "#DIV/0!", "#NOMBRE?", "#REF", "Err:522"))
+                       
+source(paste0(getwd(),'/f_clusteringBySurface_v3_para_matchratings_for_github.R'))
+
+out <- f_clusterBySurface(trainYears = c(2010:2019), 
+                                  numSurfClusters = 6,
+                                  clusterMethod = 'kmeans',
+                                  distanceMethod = 'ward.D',
+                                  data = dataIN,
+                                  minReg = 40,
+                                  generar_clusters = T
+)
