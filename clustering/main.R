943b062a11a3919d6b54b88887d90607e8b5e7c0
cvalienteDS <65561135+cvalienteDS@users.noreply.github.com>
2020-05-19 15:12:07 +0200

    Update main.R

--- clustering/main.R
+++ clustering/main.R
@@ -8,7 +8,7 @@ dataIN <- read.csv('toy_sample.csv',
                        stringsAsFactors = F,
                        na.strings=c("#N/D", "#VALOR","#¡VALOR!", "#VALOR!", "#DIV/0!", "#NOMBRE?", "#REF", "Err:522"))
                        
-source(paste0(getwd(),'/f_clusteringBySurface_v3_para_matchratings_for_github.R'))
+source(paste0(getwd(),'/f_clusteringBySurface_for_github.R'))
 
 out <- f_clusterBySurface(trainYears = c(2010:2019), 
                                   numSurfClusters = 6,
